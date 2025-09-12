b94b48baaf117e6ae77cf2d2b9bc5a5706e5e106
pablorojasfernandez <pablo.rojas.fernandez@alumnos.upm.es>
2025-09-12 20:06:31 +0200

    Commit 3 #1

--- src/main/java/es/upm/miw/devops/Application.java
+++ src/main/java/es/upm/miw/devops/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}) // Not API: /error
 public class Application {
-    // Commit 2
+    // Commit 3
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);// mvn clean spring-boot:run
     }
