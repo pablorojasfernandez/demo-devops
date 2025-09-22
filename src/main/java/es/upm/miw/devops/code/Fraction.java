e36bb15907c52d35008a9bcb8e24ca0b669b56c3
pablorojasfernandez <pablo.rojas.fernandez@alumnos.upm.es>
2025-09-22 20:15:04 +0200

    Clases

--- src/main/java/es/upm/miw/devops/code/Fraction.java
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -1,4 +1,96 @@
 package es.upm.miw.devops.code;
 
+/**
+ * Conceptos: Las fracciones propias son aquellas cuyo numerador es menor que el denominador
+ * <p>
+ * Las fracciones impropias son aquellas cuyo numerador es mayor que el denominador
+ * <p>
+ * Dos fracciones son equivalentes cuando el producto de extremos (numerador de la primera por denominador de la segunda) es igual al
+ * producto de medios (denominador de la primera por el numerador de la segunda)
+ * <p>
+ * Las fracciones irreducibles son aquellas que no se pueden simplificar, esto sucede cuando el numerador y el denominador son primos entre
+ * sí
+ * <p>
+ * Reducir varias fracciones a común denominador consiste en convertirlas en otras equivalentes que tengan el mismo denominador
+ * <p>
+ * Comparar fracciones
+ * <p>
+ * Suma fracciones: En primer lugar se reducen los denominadores a común denominador, y se suman o se restan los numeradores de las
+ * fracciones equivalentes obtenidas
+ * <p>
+ * Multiplicación: La multiplicación de dos fracciones es otra fracción que tiene: Por numerador el producto de los numeradores. Por
+ * denominador el producto de los denominadores.
+ * <p>
+ * La división de dos fracciones es otra fracción que tiene: Por numerador el producto de los extremos. Por denominador el producto de los
+ * medios. Invertir fraccion
+ */
 public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+    public boolean isProper() {
+        return Math.abs(this.numerator) < Math.abs(this.denominator);
+    }
+
+    public boolean isImproper() {
+        return Math.abs(this.numerator) >= Math.abs(this.denominator);
+    }
+
+    public boolean isEquivalent(Fraction fraction) {
+        return this.numerator * fraction.getDenominator() == this.denominator * fraction.getNumerator();
+    }
+
+    public Fraction add(Fraction other) {
+        int commonDenominator = this.denominator * other.getDenominator();
+        int newNumerator = this.numerator * other.getDenominator() + other.getNumerator() * this.denominator;
+        return new Fraction(newNumerator, commonDenominator);
+    }
+
+    public Fraction multiply(Fraction other) {
+        return new Fraction(this.numerator * other.getNumerator(), this.denominator * other.getDenominator());
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.getNumerator() == 0) {
+            throw new ArithmeticException("Cannot divide by a fraction with numerator 0");
+        }
+        return new Fraction(this.numerator * other.getDenominator(), this.denominator * other.getNumerator());
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
 }
